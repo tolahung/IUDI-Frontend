e612a9830c12c579d4db0a35254722826935d7fb
tolahung <idoky19@gmail.com>
2024-03-04 11:40:40 +0700

    upadate

--- src/components/Home/Layout.jsx
+++ src/components/Home/Layout.jsx
@@ -6,7 +6,7 @@ function LayoutHome() {
         <div className="">
             <div className="flex flex-col  justify-center items-center h-screen mt-[-117px]">
                 <img src= {Logo} alt="Your Image" className="max-w-full max-h-full w-68 h-48 min-w-200" />
-                <h1 className = "text-white text-7xl">Kết Nối Yêu Thương</h1>
+                <h1 className = "text-white text-7xl font-bold">Kết Nối Yêu Thương</h1>
             </div>
         </div>
     )
