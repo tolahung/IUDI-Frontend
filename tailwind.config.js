b45680992aaa512b1c08d170064750f3ed89e204
Beo kutee <hunglcvph28989@fpt.edu.vn>
2024-03-14 22:47:27 +0700

    change profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [require("daisyui")],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -8,6 +9,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
 }
 
